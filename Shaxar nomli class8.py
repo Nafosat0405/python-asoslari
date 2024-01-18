9167cbad3e461c12e1ca822e8db67e189d97eccd
Nafosat0405 <rustambekovanafosat69@gmail.com>
2024-01-18 09:13:27 +0300

    Create Shaxar nomli class8.py

---
+++ Shaxar nomli class8.py
@@ -0,0 +1,17 @@
+class Shaxar:
+    def __init__(self,nomi,respublikasi,xuquqiy_xolati):
+        self.nomi=nomi
+        self.respublikasi=respublikasi
+        self.xuquqiy_xolati=xuquqiy_xolati
+        
+    def get_info(self):
+        return f"{self.nomi},{self.respublikasi}.{self.xuquqiy_xolati}"
+    
+    def set_nomi(self,x):
+        self.nomi=x
+        
+obj1=Shaxar("Urganch","uzbekiston","Urganch","demokratik")
+        
+    
+        
+        
